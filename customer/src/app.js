c20d82658fc930b827ed1caddd487d73d9610933
Piyush Gupta <pag12344321@gmail.com>
2024-05-05 22:07:56 +0530

    deployment files added

--- customer/src/app.js
+++ customer/src/app.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(config.db_uri, {})
+        await mongoose.connect('mongodb+srv://guptapiyush1048:hwoz7qDgZ1U4gqE4@cluster0.uegm7g9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {})
     }catch (e) {
         console.log(e);
         process.exit(1)
